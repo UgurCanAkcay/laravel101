7be1eb7e044b2607e6672dde6b58f8ce92a30cae
Ugur Can Akcay <51748845+UgurCanAkcay@users.noreply.github.com>
2021-08-05 16:20:27 +0300

    Add files via upload

---
+++ resources/views/ornek.blade.php
@@ -0,0 +1,6 @@
+Merhaba <?php //echo $ad; ?>,
+Merhaba {{$ad}},
+<?php echo $ad;
+ //http://localhost:8000/phpturkiye/mehmet
+ // ilk iki satir ayni islevde
+  ?>
---
+++ resources/views/web.blade.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Laravel - PHP Türkiye</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="https://tasarim.phpturkiye.net/assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <!-- Third party plugin CSS-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="https://tasarim.phpturkiye.net/css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="{{route('karegen')}}">KaregenSite</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Hakkımızda</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Hizmetler</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Features of Hotels</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">İletişim</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end">
+                        <h1 class="text-uppercase text-white font-weight-bold">{{$yazi1}}</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                        <p class="text-white-75 font-weight-light mb-5">{{$yazi2}}</p>
+                        <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Devam Et</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="page-section bg-primary" id="about">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="text-white mt-0">{{$yazi3}}</h2>
+                        <hr class="divider light my-4" />
+                        <p class="text-white-50 mb-4">{{$yazi4}}</p>
+                        <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">{{$yazi5}}</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Services-->
+        <section class="page-section" id="services">
+            <div class="container">
+                <h2 class="text-center mt-0">At Your Service</h2>
+                <hr class="divider my-4" />
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-4x fa-calendar text-primary mb-4"></i>
+                            <h3 class="h4 mb-2">Reservation</h3>
+                            <p class="text-muted mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-4x fa-map text-primary mb-4"></i>
+                            <h3 class="h4 mb-2">Location</h3>
+                            <p class="text-muted mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-4x fa-plane text-primary mb-4"></i>
+                            <h3 class="h4 mb-2">Transport</h3>
+                            <p class="text-muted mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-4x fa-info text-primary mb-4"></i>
+                            <h3 class="h4 mb-2">Info</h3>
+                            <p class="text-muted mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Portfolio-->
+        <div id="portfolio">
+            <div class="container-fluid p-0">
+                <div class="row no-gutters">
+                    <div class="col-lg-3 col-sm-6">
+                        <a class="portfolio-box" href="https://raw.githubusercontent.com/UgurCanAkcay/laravel101/main/img/features%20(1).jpg">
+                            <img class="img-fluid" src="https://raw.githubusercontent.com/UgurCanAkcay/laravel101/main/img/features%20(1).jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Bakım</div>
+                                <div class="project-name">Özel Yağlar ile Masaj.</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <a class="portfolio-box" href="https://raw.githubusercontent.com/UgurCanAkcay/laravel101/main/img/features%20(2).jpg">
+                            <img class="img-fluid" src="https://raw.githubusercontent.com/UgurCanAkcay/laravel101/main/img/features%20(2).jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Bakım</div>
+                                <div class="project-name">Cilt Temizleme</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <a class="portfolio-box" href="https://raw.githubusercontent.com/UgurCanAkcay/laravel101/main/img/features%20(3).jpg">
+                            <img class="img-fluid" src="https://raw.githubusercontent.com/UgurCanAkcay/laravel101/main/img/features%20(3).jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Yemek</div>
+                                <div class="project-name">Usta Aşçıların Özel Yemekleri</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <a class="portfolio-box" href="https://raw.githubusercontent.com/UgurCanAkcay/laravel101/main/img/features%20(4).jpg">
+                            <img class="img-fluid" src="https://raw.githubusercontent.com/UgurCanAkcay/laravel101/main/img/features%20(4).jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Masaj</div>
+                                <div class="project-name">Yerel/Bölgesel Masah Türleri</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <a class="portfolio-box" href="https://raw.githubusercontent.com/UgurCanAkcay/laravel101/main/img/features%20(5).jpg">
+                            <img class="img-fluid" src="https://raw.githubusercontent.com/UgurCanAkcay/laravel101/main/img/features%20(5).jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Eğlence</div>
+                                <div class="project-name">Manzara Eşliğinde Havuz Keyfi</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <a class="portfolio-box" href="https://raw.githubusercontent.com/UgurCanAkcay/laravel101/main/img/features%20(6).jpg">
+                            <img class="img-fluid" src="https://raw.githubusercontent.com/UgurCanAkcay/laravel101/main/img/features%20(6).jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Eğlence</div>
+                                <div class="project-name">Night Show</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <a class="portfolio-box" href="https://raw.githubusercontent.com/UgurCanAkcay/laravel101/main/img/features%20(7).jpg">
+                            <img class="img-fluid" src="https://raw.githubusercontent.com/UgurCanAkcay/laravel101/main/img/features%20(7).jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Barınma</div>
+                                <div class="project-name">Hijyenik ve Özenle Hazırlanmış Odalar</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <a class="portfolio-box" href="https://raw.githubusercontent.com/UgurCanAkcay/laravel101/main/img/features%20(8).jpg">
+                            <img class="img-fluid" src="https://raw.githubusercontent.com/UgurCanAkcay/laravel101/main/img/features%20(8).jpg" alt="" />
+                            <div class="portfolio-box-caption p-3">
+                                <div class="project-category text-white-50">Eğlence</div>
+                                <div class="project-name">Spa&Sauna Odaları</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Call to action-->
+
+        <!-- Contact-->
+        <section class="page-section" id="contact">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="mt-0">Let's Get In Touch!</h2>
+                        <hr class="divider my-4" />
+                        <p class="text-muted mb-5">Ready to start your next project with us? Give us a call or send us an email and we will get back to you as soon as possible!</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+                        <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+                        <div>+1 (555) 123-4567</div>
+                    </div>
+                    <div class="col-lg-4 mr-auto text-center">
+                        <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+                        <!-- Make sure to change the email address in BOTH the anchor text and the link target below!-->
+                        <a class="d-block" href="mailto:contact@yourwebsite.com">contact@yourwebsite.com</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="bg-light py-5">
+            <div class="container"><div class="small text-center text-muted">Copyright © 2021 - KaregenSite</div></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="https://tasarim.phpturkiye.net/js/scripts.js"></script>
+    </body>
+</html>
