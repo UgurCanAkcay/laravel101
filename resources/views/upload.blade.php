c8f169d4a09ff59f5f4d886b266d53d11ad64dd3
Ugur Can Akcay <51748845+UgurCanAkcay@users.noreply.github.com>
2021-08-09 18:01:15 +0300

    Add files via upload

---
+++ resources/views/upload.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Resim Upload Uygulaması</title>
+</head>
+<body>
+
+<form action="{{route('yukle')}}" method="post" enctype="multipart/form-data">
+   @csrf
+   <label>Resim Seçiniz</label><br>
+   <input type="file" name="resim"><br><br><br><br>
+   <input type="submit" name="ilet" value="Resim Yükle">
+
+
+</form>
+
+</body>
+</html>
